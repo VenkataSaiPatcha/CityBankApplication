bdb491e77b4b09c5751b1123370b48f330e840a5
TL <TL@gmail.com>
2025-06-29 12:41:03 +0530

    Dev01 modified Test.java

--- DemoProj02OfGit/src/com/sai/Test/Test.java
+++ DemoProj02OfGit/src/com/sai/Test/Test.java
@@ -1,5 +1,15 @@
 package com.sai.Test;
 
-public class Test {
+import com.sai.Beans.UtilityClass;
+
+public class Test 
+{
+	public static void main(String[] args) 
+	{
+		UtilityClass utility = new UtilityClass();
+		System.out.println("Test.main()");
+		System.out.println(utility.newSysDate());
+		
+	}
 
 }
